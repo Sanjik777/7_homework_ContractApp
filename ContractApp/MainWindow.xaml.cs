bea62d3db28793db4cbd5ac32ac7a410620c7e3e
Sanjik <Beiseev_193@mail.ru>
2019-05-19 23:33:37 +0600

    v1

---
+++ ContractApp/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Printing;
+using System.Windows.Controls;
+
+namespace ContractApp
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void PrintButtonClick(object sender, RoutedEventArgs e)
+		{
+			PrintDialog printDialog = new PrintDialog();
+			if (printDialog.ShowDialog() == true)
+			{
+				printDialog.PrintVisual(gridElement, "Распечатываем");
+			}
+		}
+	}
+}
